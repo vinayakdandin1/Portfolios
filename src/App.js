1025254c38e2281b82c4b64550bf1fc9dad5bcce
Vinayak Dandin <vinayakdandin1@gmail.com>
2021-10-21 23:12:42 +0200

    Basic Navbar setup complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Switch, withRouter } from 'react-router-dom';
-import Navbar from './components/Navbar';
+
 import AllProjects from './components/AllProjects';
 import Landing from './components/Landing';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navigation />
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/allProjects" component ={AllProjects} />
